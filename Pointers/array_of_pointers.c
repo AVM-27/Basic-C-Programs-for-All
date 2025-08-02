7a8e5530f6a0ec2c4c0ac196fb2c34f2e6967d9a
ADARSH VENUGOPAL <adarsh.venugopal.2@gmail.com>
2025-08-02 13:30:00 +0530

    Update array_of_pointers.c

--- Pointers/array_of_pointers.c
+++ Pointers/array_of_pointers.c
@@ -4,6 +4,11 @@
  * Author      : Adarsh Venugopal (https://github.com/AVM-27)
  * Date        : 2025-08-02
  *
+ * Functionality:
+ * - Declares an integer array `var` with 3 elements.
+ * - Creates an array of pointers `ptr[]`, where each element stores the address of an element in `var`.
+ * - Prints values of `var[]` using pointer dereferencing.
+ *
  * Sample Output:
  * Value of var[0] = 10
  * Value of var[1] = 100
@@ -12,19 +17,19 @@
 
 #include <stdio.h>
 
-const int MAX = 3;
+#define MAX 3
 
 int main() {
-    int var[MAX] = {10, 100, 200};  // Array of integers
-    int *ptr[MAX];                  // Array of pointers to int
+    int var[MAX] = {10, 100, 200};  // Integer array
+    int *ptr[MAX];                  // Array of pointers to integers
     int i;
 
-    // Assign each pointer the address of the corresponding element in var
+    // Assign address of each element in var[] to ptr[]
     for(i = 0; i < MAX; i++) {
         ptr[i] = &var[i];
     }
 
-    // Print the values using pointers
+    // Print values using the array of pointers
     for(i = 0; i < MAX; i++) {
         printf("Value of var[%d] = %d\n", i, *ptr[i]);
     }
