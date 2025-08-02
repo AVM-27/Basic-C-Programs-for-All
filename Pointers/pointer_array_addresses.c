66e0ea416515064dbc0473d77576a5008cd16ceb
ADARSH VENUGOPAL <adarsh.venugopal.2@gmail.com>
2025-08-02 13:33:29 +0530

    Update pointer_array_addresses.c

--- Pointers/pointer_array_addresses.c
+++ Pointers/pointer_array_addresses.c
@@ -1,37 +1,50 @@
 /*
  * Filename    : pointers/pointer_array_addresses.c
- * Description : Takes an array input from the user and prints the memory addresses of each element.
+ * Description : 
+ *   This program accepts an integer array from the user (using Variable Length Arrays)
+ *   and prints the memory addresses of each element using pointer notation. 
+ *   It helps demonstrate how arrays are stored in contiguous memory and shows
+ *   how to access the address of individual elements using pointers.
+ *
  * Author      : Adarsh Venugopal (https://github.com/AVM-27)
  * Date        : 2025-08-02
  *
- * Sample Output:
+ * Sample Input:
  * What is the size of the array?
  * 4
  * Enter the elements
  * 10 20 30 40
+ *
+ * Sample Output:
  * The addresses where your array is stored are:
- * 6422032 6422036 6422040 6422044
+ * 0x7ffdb8d87a10 0x7ffdb8d87a14 0x7ffdb8d87a18 0x7ffdb8d87a1c
  */
 
 #include <stdio.h>
 
 int main() {
     int size;
+
+    // Prompt user to enter size of array
     printf("What is the size of the array?\n");
     scanf("%d", &size);
 
-    int x[size];  // Variable Length Array (VLA)
+    // Declare a Variable Length Array (VLA)
+    int x[size];
 
+    // Prompt to enter array elements
     printf("Enter the elements\n");
     for (int i = 0; i < size; i++) {
         scanf("%d", &x[i]);
     }
 
+    // Display addresses of each element in the array
     printf("The addresses where your array is stored are:\n");
     for (int i = 0; i < size; i++) {
-        printf("%p ", (void*)&x[i]);  // Proper address format using %p
+        // Cast to void* to properly print address using %p
+        printf("%p ", (void*)&x[i]);
     }
 
-    printf("\n");
+    printf("\n");  // For clean output formatting
     return 0;
 }
