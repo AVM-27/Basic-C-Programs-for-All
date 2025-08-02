dea43c88304fef6a90f3a1842c3a4efe56811c16
ADARSH VENUGOPAL <adarsh.venugopal.2@gmail.com>
2025-08-02 13:34:42 +0530

    Update pointers_and_strings.c

--- Pointers/pointers_and_strings.c
+++ Pointers/pointers_and_strings.c
@@ -1,6 +1,11 @@
 /*
  * Filename    : Pointers/pointers_and_strings.c
- * Description : Demonstrates traversal of a string using a character pointer.
+ * Description : 
+ *   Demonstrates how a character pointer can be used to traverse
+ *   a null-terminated string in C.
+ *   Instead of using array indexing, pointer arithmetic is used
+ *   to access each character until the end of the string.
+ *
  * Author      : Adarsh Venugopal (https://github.com/AVM-27)
  * Date        : 2025-08-02
  *
@@ -16,10 +21,10 @@ int main() {
     char *p = str;               // Pointer to the start of the string
     int i;
 
-    // Traverse and print each character using pointer
+    // Traverse and print each character using the pointer
     for(i = 0; i < strlen(str); i++) {
-        printf("%c", *p);
-        p++;
+        printf("%c", *p);  // Print current character
+        p++;               // Move to next character
     }
 
     printf("\n");
