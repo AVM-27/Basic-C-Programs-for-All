a25ddd3b50731ef559b41286dac58a2e14c9e5cb
ADARSH VENUGOPAL <adarsh.venugopal.2@gmail.com>
2025-08-02 13:35:26 +0530

    Update ptrfunc.c

--- Pointers/ptrfunc.c
+++ Pointers/ptrfunc.c
@@ -1,6 +1,10 @@
 /*
  * Filename    : pointers/ptrfunc.c
- * Description : Swaps two integers using pointers.
+ * Description : 
+ *   Swaps two integers using pointers.
+ *   Demonstrates how function arguments passed by address allow
+ *   the function to modify the original variables.
+ *
  * Author      : Adarsh Venugopal (https://github.com/AVM-27)
  * Date        : 2025-08-02
  *
@@ -13,17 +17,19 @@
 // Function to exchange values of two integers via pointers
 void exchange(int *px, int *py) {
     int temp;
-    temp = *px;
-    *px = *py;
-    *py = temp;
+    temp = *px;   // Store value pointed to by px
+    *px = *py;    // Assign value pointed to by py to px
+    *py = temp;   // Assign original px value to py
 }
 
 int main() {
     int a = 5;
     int b = 7;
 
+    // Swap values of a and b using their addresses
     exchange(&a, &b);
 
+    // Print swapped values
     printf("%d %d\n", a, b);
 
     return 0;
