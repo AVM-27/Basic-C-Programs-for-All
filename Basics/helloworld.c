35e0e4f5f67328aa51e04d619da921cef607a042
ADARSH VENUGOPAL <adarsh.venugopal.2@gmail.com>
2025-08-02 10:06:04 +0530

    Create helloworld.c

---
+++ Basics/helloworld.c
@@ -0,0 +1,18 @@
+/*
+ * File: helloworld.c
+ * ------------------
+ * Note: A straightforward program to demonstrate printing multiple
+ * lines of text to the console using the printf function.
+ */
+#include <stdio.h> // Includes the Standard Input/Output Library for printf.
+
+int main() { // The main function where execution begins.
+
+    // Print the first line of text. The '\n' character creates a new line.
+    printf("Hello World\n");
+
+    // Print the second line of text.
+    printf("I want the rocket to be pointhy pointhy\n");
+
+    return 0; // Return 0 to indicate the program ran successfully.
+}
