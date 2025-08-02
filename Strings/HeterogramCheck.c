e923f7f0dfe323231f69c8eb0bd2b61abb857620
ADARSH VENUGOPAL <adarsh.venugopal.2@gmail.com>
2025-08-02 14:46:28 +0530

    Update HeterogramCheck.c

--- Strings/HeterogramCheck.c
+++ Strings/HeterogramCheck.c
@@ -1,45 +1,41 @@
 /*
  * Filename    : Strings/HeterogramCheck.c
- * Description : Checks if a given word is a heterogram (no repeating characters).
- * Author      : Adarsh Venugopal (https://github.com/AVM-27)
- * Date        : 2025-08-02
+ * Description : Checks whether a given string is a heterogram (no repeated characters).
+ * Author      : Adarsh Venugopal
  *
- * What is a Heterogram?
- *  - A heterogram is a word or phrase in which no letter occurs more than once.
+ * A heterogram is a word or sentence in which no letter occurs more than once.
  *
- * Functionality:
- *  - Accepts a string from the user (without spaces).
- *  - Sorts the string alphabetically using insertion sort.
- *  - Checks for repeated characters.
- *  - Prints whether it is a heterogram.
+ * ---------------------------
+ * Sample Input/Output:
+ * ---------------------------
+ * Enter the size of your array:
+ * 6
+ * Enter the elements of your array:
+ * planet
+ * Output: IT IS A HETEROGRAM
  *
- * Sample I/O:
- *  Enter a word:
- *  lamp
- *  IT IS A HETEROGRAM
- *
- *  Enter a word:
- *  apple
- *  IT IS NOT A HETEROGRAM
+ * Enter the elements of your array:
+ * apple
+ * Output: IT IS NOT A HETEROGRAM
  */
 
 #include <stdio.h>
 #include <string.h>
 
 int main() {
-    char arr[100], key;
-    int i, j, is_duplicate = 0;
-    int num;
+    int i, j, duplicateCount = 0;
+    int size;
 
-    printf("Enter the number of characters in your word: ");
-    scanf("%d", &num);
+    printf("Enter the size of your string:\n");
+    scanf("%d", &size);
 
-    printf("Enter the word (no spaces): ");
+    char arr[size];
+    printf("Enter the elements of your array (string):\n");
     scanf("%s", arr);
 
-    // Insertion sort to arrange characters
-    for (i = 1; i < num; i++) {
-        key = arr[i];
+    // Sorting the array to bring duplicate letters together
+    for (i = 1; i < size; i++) {
+        char key = arr[i];
         j = i - 1;
         while (j >= 0 && arr[j] > key) {
             arr[j + 1] = arr[j];
@@ -48,15 +44,15 @@ int main() {
         arr[j + 1] = key;
     }
 
-    // Check for duplicates in sorted array
-    for (i = 0; i < num - 1; i++) {
+    // Checking for duplicate characters
+    for (i = 0; i < size - 1; i++) {
         if (arr[i] == arr[i + 1]) {
-            is_duplicate = 1;
+            duplicateCount++;
             break;
         }
     }
 
-    if (is_duplicate == 0) {
+    if (duplicateCount == 0) {
         printf("IT IS A HETEROGRAM\n");
     } else {
         printf("IT IS NOT A HETEROGRAM\n");
